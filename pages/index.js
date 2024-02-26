b2493da852701c6eaed70b46cfb439f2526076fd
BRUNNO <30895581+BrunnoML@users.noreply.github.com>
2024-02-26 15:25:29 +0000

    adiciona o arquivo index

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>"Não espere pela oportunidade, crie-a.\nNão espere pela motivação, seja você a motivação.\nNão espere pela permissão, dê a si mesmo o aval para viver plenamente.\nA vida é uma jornada única, faça valer a pena cada momento.\nAutor: ChatGPT\n\nO perfeito é inimigo do feito!!\nAutor: meu pai inspirado em Voltaire"</h1>
+}
+
+export default Home;
