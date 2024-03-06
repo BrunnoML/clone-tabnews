fcd35c8b57757a4f22d6978a6bfd32cd62fa0215
BRUNNO <brunnoml@gmail.com>
2024-03-06 00:13:50 -0300

    feature: Adiciona scripts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 function Home() {
-    return <h1>
-        <pre>
-            {"Não espere pela oportunidade, crie-a.\nNão espere pela motivação, seja você a motivação.\nNão espere pela permissão, dê a si mesmo o aval para viver plenamente.\nA vida é uma jornada única, faça valer a pena cada momento.\nAutor: ChatGPT\n\nO perfeito é inimigo do feito!!\nAutor: meu pai inspirado em Voltaire"}
-            </pre>
-            </h1>
+  return (
+    <h1>
+      <pre>
+        {
+          "Não espere pela oportunidade, crie-a.\nNão espere pela motivação, seja você a motivação.\nNão espere pela permissão, dê a si mesmo o aval para viver plenamente.\nA vida é uma jornada única, faça valer a pena cada momento.\nAutor: ChatGPT\n\nO perfeito é inimigo do feito!!\nAutor: meu pai inspirado em Voltaire"
+        }
+      </pre>
+    </h1>
+  );
+  function teste() {
+    console.log("teste");
+  }
 }
 
 export default Home;
